1f8d4cfc0a17f904b4b71c0d891d7c607c95cf16
Stéphane Derosiaux <derste@gmail.com>
2017-02-04 14:56:53 +0100

    Tweak headroom style

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -44,8 +44,7 @@ module.exports = React.createClass({
                     </Box>
                     <Box px={0}>
                         <form style={{margin:0,padding:0,fontSize:14}} onSubmit={this.handleSubmit} action="//ctheu.us15.list-manage.com/subscribe/post?u=10fa09a0a644f563c084ab3fd&amp;id=32c9c3f4b2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
-                          <input style={{height: 26}} type="email" value={this.state.email} onChange={this.handleChange} name="EMAIL" placeholder="my.email@domain.com" id="mce-EMAIL" />
-                          &nbsp;
+                          <input style={{height: 26, lineHeight: 1, borderRight: 0}} type="email" value={this.state.email} onChange={this.handleChange} name="EMAIL" placeholder="my.email@domain.com" id="mce-EMAIL" />
                           <input style={{height: 26, lineHeight: 1}} type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" />
                           <div style={{position: "absolute", left: -5000}} aria-hidden="true"><input type="text" name="b_10fa09a0a644f563c084ab3fd_32c9c3f4b2" tabindex="-1" value="" /></div>
                         </form>
