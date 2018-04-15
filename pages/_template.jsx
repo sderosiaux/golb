8bd2cbed83243eed6ca107eca33ded7abd76ecc6
Stéphane Derosiaux <derste@gmail.com>
2018-04-15 16:55:55 +0200

    More and more style

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -13,6 +13,10 @@ import img from './up_arrow_round.png'
 import { SocialIcon } from 'react-social-icons'
 
 module.exports = React.createClass({
+  contextTypes: {
+    location: React.PropTypes.object
+  },
+
   getInitialState: function () {
     return { email: '' }
   },
@@ -28,31 +32,29 @@ module.exports = React.createClass({
     setTimeout(() => this.setState({ email: '' }), 100);
   },
   render: function () {
-
+    const pathname = this.context.location.pathname
     return (
       <div>
         <Headroom wrapperStyle={{ marginBottom: rhythm(1) }}>
           <Container style={{ maxWidth: null, padding: '16px 20px' }}>
             <Flex align="center" col="3" justify="space-between">
               <Box>
-                <div style={{ width: 300 }}>
-                  <Flex>
-                    <Box px={1}>
-                      <a target="_blank" href="https://twitter.com/ChtefiD" style={{ color: 'black', textDecoration: 'none', }}>
-                        <img src={m} height={32} style={{ borderRadius: 100, margin: 0, display: 'block' }} />
-                      </a>
-                    </Box>
-                    <Box px={0}>
-                      <form style={{ margin: 0, padding: 0, fontSize: 14 }} onSubmit={this.handleSubmit} action="//ctheu.us15.list-manage.com/subscribe/post?u=10fa09a0a644f563c084ab3fd&amp;id=32c9c3f4b2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
-                        <input type="email" value={this.state.email} onChange={this.handleChange} name="EMAIL" placeholder="my.email@domain.com" id="mce-EMAIL" />
-                        <input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" />
-                        <div style={{ position: "absolute", left: -5000 }} aria-hidden="true"><input type="text" name="b_10fa09a0a644f563c084ab3fd_32c9c3f4b2" tabindex="-1" value="" /></div>
-                      </form>
-                    </Box>
-                  </Flex>
-                </div>
+                <Flex>
+                  <Box px={1}>
+                    <a target="_blank" href="https://twitter.com/ChtefiD" style={{ color: 'black', textDecoration: 'none', }}>
+                      <img src={m} height={32} style={{ borderRadius: 100, margin: 0, display: 'block' }} />
+                    </a>
+                  </Box>
+                  <Box px={0}>
+                    <form style={{ margin: 0, padding: 0, fontSize: 14 }} onSubmit={this.handleSubmit} action="//ctheu.us15.list-manage.com/subscribe/post?u=10fa09a0a644f563c084ab3fd&amp;id=32c9c3f4b2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
+                      <input type="email" value={this.state.email} onChange={this.handleChange} name="EMAIL" placeholder="my.email@domain.com" id="mce-EMAIL" />
+                      <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" />
+                      <div style={{ position: "absolute", left: -5000 }} aria-hidden="true"><input type="text" name="b_10fa09a0a644f563c084ab3fd_32c9c3f4b2" tabindex="-1" value="" /></div>
+                    </form>
+                  </Box>
+                </Flex>
               </Box>
-              <Box px={1} className="index"><Link to={prefixLink('/')}>Latest articles</Link></Box>
+              {pathname !== '/' && <Box px={1} className="index">Check out my <Link to={prefixLink('/')}><span className="nice-btn">Latest articles</span></Link></Box>}
               <Box px={1} className="social">
                 <div>
                   <SocialIcon url="https://twitter.com/ChtefiD" style={{ height: 32, width: 32 }} />&nbsp;
