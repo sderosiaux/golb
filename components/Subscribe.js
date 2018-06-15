0b1825d420c8e590db365b56602398eb35d313f4
Stéphane Derosiaux <derste@gmail.com>
2018-06-15 15:09:05 +0200

    Change some texts

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -2,30 +2,50 @@ import React from 'react'
 import { prefixLink } from 'gatsby-helpers'
 
 export default class Subscribe extends React.Component {
-    constructor() {
-        super()
-        this.state = { email: '' }
-    }
-    handleChange(event) {
-        this.setState({ email: event.target.value })
-    }
-    handleSubmit() {
-        setTimeout(() => this.setState({ email: '' }), 100)
-    }
-    render() {
-        return (
-            <div className="newsletter">
-                <h1>Subscribe to my newsletter</h1>
-                <p>If you like what you read, don't hesitate to join the club and receive an email when I post a new article! No spam ever, I promise. ❤❤❤</p>
-                <form onSubmit={() => this.handleSubmit()} action="//ctheu.us15.list-manage.com/subscribe/post?u=10fa09a0a644f563c084ab3fd&amp;id=32c9c3f4b2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
-                    <div><input type="email" value={this.state.email} onChange={(e) => this.handleChange(e)} name="EMAIL" placeholder="my.email@domain.com" /></div>
-                    <div><input type="submit" className="nice-btn" value="I subscribe" name="subscribe" /></div>
-                    <div style={{ position: "absolute", left: -5000 }} aria-hidden="true"><input type="text" name="b_10fa09a0a644f563c084ab3fd_32c9c3f4b2" tabindex="-1" value="" /></div>
-                </form>
-            </div>
-        )
-    }
+  constructor() {
+    super()
+    this.state = { email: '' }
+  }
+  handleChange(event) {
+    this.setState({ email: event.target.value })
+  }
+  handleSubmit() {
+    setTimeout(() => this.setState({ email: '' }), 100)
+  }
+  render() {
+    return (
+      <div className="newsletter">
+        <h1>I like what I read, thanks!</h1>
+        <p>
+          Don't hesitate to join the club and receive an email when I post a new article! No spam ever, I promise. ❤❤❤
+        </p>
+        <form
+          onSubmit={() => this.handleSubmit()}
+          action="//ctheu.us15.list-manage.com/subscribe/post?u=10fa09a0a644f563c084ab3fd&amp;id=32c9c3f4b2"
+          method="post"
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          className="validate"
+          target="_blank"
+          novalidate
+        >
+          <div>
+            <input
+              type="email"
+              value={this.state.email}
+              onChange={e => this.handleChange(e)}
+              name="EMAIL"
+              placeholder="my.email@domain.com"
+            />
+          </div>
+          <div>
+            <input type="submit" className="nice-btn" value="Keep me in touch" name="subscribe" />
+          </div>
+          <div style={{ position: 'absolute', left: -5000 }} aria-hidden="true">
+            <input type="text" name="b_10fa09a0a644f563c084ab3fd_32c9c3f4b2" tabindex="-1" value="" />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
-
-
-
--- pages/index.js
+++ pages/index.js
@@ -10,41 +10,49 @@ import compareDesc from 'date-fns/compare_desc'
 import { Flex, Box } from 'reflexbox'
 import { SocialIcon } from 'react-social-icons'
 
-import "css/index"
+import 'css/index'
 
-export default (props) => {
-
-  const blogs = props.route.pages.filter(page => page.data.layout == 'post')
+export default props => {
+  const blogs = props.route.pages
+    .filter(page => page.data.layout == 'post')
     .map(page => page.data)
     .sort((a, b) => compareDesc(a.date, b.date))
 
   return (
     <div>
-      <Helmet
-        title={config.siteTitle}
-        link={[
-          { "rel": "icon", "href": "/m.jpg" }
-        ]}
-      />
+      <Helmet title={config.siteTitle} link={[{ rel: 'icon', href: '/m.jpg' }]} />
       <div className="my-presentation">
         <h2>Bonjour,</h2>
-        <p>I'm Stéphane, a Senior Scala and Data Engineer from France, and previously Front-End Developer. Passionated about everything related to IT, I <a href="https://github.com/sderosiaux/every-single-day-i-tldr">read a lot</a> and try to provide you some feedback about what I learned. I hope you'll enjoy it.</p>
-        <div>Feel free to come say Hi <SocialIcon url="https://twitter.com/sderosiaux" style={{ height: 32, width: 32, verticalAlign: 'text-top' }} /></div>
+        <p>
+          I'm Stéphane, a Senior Scala and Data Engineer from France, and previously Front-End Developer. Passionated
+          about everything related to IT, I{' '}
+          <a href="https://github.com/sderosiaux/every-single-day-i-tldr">read a lot</a> and try to provide you some
+          feedback about what I learned. I hope you'll enjoy it.
+        </p>
+        <div>
+          Feel free to come say Hi{' '}
+          <SocialIcon
+            url="https://twitter.com/sderosiaux"
+            style={{ height: 32, width: 32, verticalAlign: 'text-top' }}
+          />
+        </div>
       </div>
-      <div style={{
-        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 120px',
-        borderRadius: '1rem',
-        padding: '10px 80px'
-      }}>
+      <div className="list-articles">
         <div className="latest">Latest articles</div>
         <ul className="index">
-          {blogs.map(blog => <li>
-            <Link className="title" to={prefixLink(blog.path)}>{blog.title}</Link>
-            <div>
-              <span className="subtitle">{format(blog.date, 'DD MMM YYYY')} — <ReadTime text={blog.body} /></span>
-              {blog.tags && <div className="tags">➡️ {blog.tags.split(',').map(t => <span>{t}</span>)}</div>}
-            </div>
-          </li>)}
+          {blogs.map(blog => (
+            <li>
+              <Link className="title" to={prefixLink(blog.path)}>
+                {blog.title}
+              </Link>
+              <div>
+                <span className="subtitle">
+                  {format(blog.date, 'DD MMM YYYY')} — <ReadTime text={blog.body} />
+                </span>
+                {blog.tags && <div className="tags">➡️ {blog.tags.split(',').map(t => <span>{t}</span>)}</div>}
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
